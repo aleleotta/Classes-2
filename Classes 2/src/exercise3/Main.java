c1c8540d101ff6cddaffaa912c7d0bbaf4c0ba22
Alessandro Leotta <alessandro.leotta@iesnervion.com>
2023-03-31 09:15:12 +0200

    Project created with all exercises

--- Classes 2/src/exercise3/Main.java
+++ Classes 2/src/exercise3/Main.java
@@ -7,3 +7,26 @@ public class Main {
 		sc.close();
 	}
 }
+
+/*Crear una clase Fecha en Java. La clase tendrá tres atributos privados: día, mes y año
+de tipo int. La clase contendrá los siguientes métodos:
+	● Constructor sin parámetros.
+	● Constructor con tres parámetros para crear objetos con valores iniciales.
+	● Métodos set y get para asignar y obtener los valores de los atributos de la
+	clase. Deben controlar que los valores a asignar son correctos.
+	● Método esBisiesto() que calcula si el año es o no bisiesto. El método
+	esBisiesto devuelve true si el año es bisiesto y false si no lo es. Este método
+	debe declararse como privado.
+	● Método fechaCorrecta() que comprueba si la fecha es correcta. Devuelve un
+	valor de tipo boolean indicando si la fecha es correcta o no. Este método a su
+	vez utilizará el método esBisiesto.
+	● Método diaSiguiente() que cambia la fecha actual por la del día siguiente. El
+	objeto de la clase Fecha al que se le aplique este método deberá quedar
+	siempre en un estado consistente, es decir, la nueva fecha deberá ser
+	correcta.
+	● Modificar el método toString() heredado de Object para mostrar las fechas de
+	la forma dd-mm-aaaa. El día y el mes se deben mostrar con dos cifras. Si el
+	dia o el mes tienen solo una cifra se escribirá un cero delante. Por ejemplo si
+	la fecha es dia=1, mes=6, año= 2015 la fecha que se mostrará será:
+	01-06-2015.
+*/
--- Classes 2/src/exercise4/Main.java
+++ Classes 2/src/exercise4/Main.java
@@ -7,3 +7,37 @@ public class Main {
 		sc.close();
 	}
 }
+
+/*Crea la clase Disco con los siguientes atributos: codigo, autor, titulo, genero de tipo
+String y duracion de tipo int. Por defecto el código tendrá el valor “LIBRE”.
+Crea los siguientes métodos:
+	● Contructores vacío, con parámetros y copia.
+	● Gettes y setters.
+	● toString.
+En el programa principal crea un CRUD para gestionar los discos de una colección:
+	1. Crea un array de discos de 10 elementos.
+	2. Para gestionar dicha colección debes crear el siguiente menú de opciones:
+
+		COLECCIÓN DE DISCOS
+		===================
+		1. Listado.
+		2. Nuevo Disco.
+		3. Modificar.
+		4. Borrar.
+		5. Salir.
+
+	3. Para listar los discos se recorre el array y muestran todos los discos que hay
+	guardados en el array. Para ello comprobamos que en su código no tenga la
+	palabra “LIBRE”.
+	4. Para añadir nuevo disco se piden los datos por teclado y se añade a la primera
+	posición del array donde esté libre. Para ello se debe crear un contador en el
+	programa principal. Este contador debe aumentar cuando insertamos un
+	disco nuevo y disminuir cuando borramos un disco y es el código con el que
+	se guarda el disco.
+	5. Para modificar un disco debemos buscarlo por su código y pedir al usuario
+	por teclado los nuevos datos del disco.
+	6. Para borrarlo debemos pedir al usuario el código del disco a borrar y poner en
+	su código la palabra “LIBRE”, así estará disponible para insertar nuevos discos.
+	7. Comprueba que funciona correctamente introduciendo valores a los discos y
+	probando las opciones del CRUD.
+*/
